2b48e989d8945a1f4ab661fa8e166c7a3793240b
metamuffin <metamuffin@disroot.org>
2023-08-20 17:20:21 +0200

    maybe working ir

--- src/bin/events.rs
+++ src/bin/events.rs
@@ -3,11 +3,24 @@
     which is licensed under the GNU Affero General Public License (version 3); see /COPYING.
     Copyright (C) 2023 metamuffin <metamuffin@disroot.org>
 */
-use wiimote_stuff::{Action, Wiimote};
+use wiimote_stuff::{Action, IRMode, ReportingMode, Wiimote};
 
 fn main() {
     env_logger::init_from_env("LOG");
     let w = Wiimote::open();
+    let a = std::env::args().nth(1);
+    if let Some(a) = a {
+        match a.as_str() {
+            "basic_ir" => {
+                w.write(Action::IRCameraEnable(Some(IRMode::Basic)));
+                w.write(Action::SetReporting(ReportingMode::ButtonsAccelIR10Ext6));
+            }
+            "accel" => {
+                w.write(Action::SetReporting(ReportingMode::ButtonsAccel));
+            }
+            _ => panic!("unknown mode"),
+        }
+    }
     loop {
         eprintln!("{:?}", w.read());
     }
--- src/lib.rs
+++ src/lib.rs
@@ -50,14 +50,27 @@ impl Wiimote {
         }
         let mut buf = [0u8; 1024];
         let size = self.device.read(&mut buf).unwrap();
-        debug!("recv {:?}", &buf[..size]);
+        trace!("recv {:?}", &buf[..size]);
         match buf[0] {
+            consts::CORE_BUTTONS_ACCELEROMETER_IR10_EXTENSION6 => {
+                out.push_back(Report::Buttons(ButtonState::from_flags([buf[1], buf[2]])));
+                out.push_back(Report::Acceleration(Acceleration::from_report(&buf)));
+                out.push_back(Report::IRDetection(IRObject::from_basic(
+                    buf[6..16].try_into().unwrap(),
+                )))
+            }
             consts::CORE_BUTTONS_ACCELEROMETER => {
                 out.push_back(Report::Buttons(ButtonState::from_flags([buf[1], buf[2]])));
+                out.push_back(Report::Acceleration(Acceleration::from_report(&buf)))
             }
             consts::CORE_BUTTONS => {
                 out.push_back(Report::Buttons(ButtonState::from_flags([buf[1], buf[2]])));
             }
+            consts::STATUS_INFORMATION => {
+                out.push_back(Report::LedState(buf[3] >> 4));
+            }
+            consts::READ_MEMORY_AND_REGISTERS_DATA => {}
+            consts::RESULT => {}
             x => {
                 warn!("unknown report type: {x:02x}");
             }
@@ -74,6 +87,7 @@ impl Wiimote {
         trace!("send {bytes:02x?}");
         self.device.write(bytes).unwrap();
     }
+
     pub fn write_registers(&self, addr: u32, data: &[u8]) {
         let mut bytes = [0; 22];
         bytes[0] = consts::WRITE_MEMORY_AND_REGISTERS;
@@ -95,7 +109,17 @@ impl Wiimote {
         match a {
             Action::SpeakerEnable(enable) => self.set_enabled(consts::SPEAKER_ENABLE, enable),
             Action::SpeakerMute(enable) => self.set_enabled(consts::SPEAKER_MUTE, enable),
-            Action::IRCameraEnable(enable) => self.set_enabled(consts::IR_CAMERA_ENABLE, enable),
+            Action::IRCameraEnable(enable) => {
+                self.set_enabled(consts::IR_CAMERA_PIXEL_CLOCK_ENABLE, enable.is_some());
+                self.set_enabled(consts::IR_CAMERA_CHIP_ENABLE, enable.is_some());
+                if let Some(mode) = enable {
+                    self.write_registers(0xb00030, &[0x80]);
+                    self.write_registers(0xb00000, &consts::IR_SENSITIVITY_LEVEL_3[0..9]);
+                    self.write_registers(0xb0001a, &consts::IR_SENSITIVITY_LEVEL_3[9..11]);
+                    self.write_registers(0xb00033, &[mode as u8]);
+                    self.write_registers(0xb00030, &[0x80]);
+                }
+            }
             Action::PlayerLeds(mask) => self.write_inner(&mut [0x11, mask << 4]),
             Action::SpeakerData(data) => {
                 let mut to_send = [0; 22];
@@ -105,6 +129,7 @@ impl Wiimote {
                 self.write_inner(&mut to_send);
                 sleep(Duration::from_millis(10));
             }
+            Action::SetReporting(r) => self.write_inner(&mut [0x12, 0x00, r as u8]),
         }
     }
 }
@@ -112,14 +137,91 @@ impl Wiimote {
 #[derive(Debug)]
 pub enum Report {
     Buttons(ButtonState),
+    Acceleration(Acceleration),
+    LedState(u8),
+    IRDetection([Option<IRObject>; 4]),
 }
 #[derive(Debug)]
 pub enum Action {
-    IRCameraEnable(bool),
+    SetReporting(ReportingMode),
+    IRCameraEnable(Option<IRMode>),
     PlayerLeds(u8),
     SpeakerEnable(bool),
     SpeakerMute(bool),
     SpeakerData([u8; 20]),
+}
+
+#[repr(u8)]
+#[derive(Debug)]
+pub enum ReportingMode {
+    Buttons = consts::CORE_BUTTONS,
+    ButtonsAccel = consts::CORE_BUTTONS_ACCELEROMETER,
+    ButtonsAccelExt16 = consts::CORE_BUTTONS_ACCELEROMETER_EXTENSION16,
+    ButtonsAccelIR10Ext6 = consts::CORE_BUTTONS_ACCELEROMETER_IR10_EXTENSION6,
+    ButtonsAccelIR12 = consts::CORE_BUTTONS_ACCELEROMETER_IR12,
+    ButtonsExt19 = consts::CORE_BUTTONS_EXTENSION19,
+    ButtonsExt8 = consts::CORE_BUTTONS_EXTENSION8,
+    ButtonsIR10Ext9 = consts::CORE_BUTTONS_IR10_EXTENSION9,
+}
+
+#[derive(Debug)]
+pub struct Acceleration {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+}
+impl Acceleration {
+    pub fn from_report(r: &[u8]) -> Self {
+        Acceleration {
+            x: (r[3] as i16 - 0x80) * 2 + (r[1] & 0b01100000) as i16 >> 5,
+            y: (r[4] as i16 - 0x80) * 2 + (r[1] & 0b00100000) as i16 >> 5,
+            z: (r[5] as i16 - 0x80) * 2 + (r[1] & 0b01000000) as i16 >> 6,
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug)]
+pub enum IRMode {
+    Basic = 1,
+    Extended = 3,
+    Full = 5,
+}
+
+#[derive(Debug)]
+pub struct IRObject {
+    pub x: u16,
+    pub y: u16,
+    pub size_or_intensity: u8,
+}
+impl IRObject {
+    fn from_basic_pair(r: [u8; 5]) -> [Option<Self>; 2] {
+        [
+            if r[0] == 0xff && r[1] == 0xff {
+                None
+            } else {
+                Some(Self {
+                    size_or_intensity: 0,
+                    x: r[0] as u16 | ((r[2] & 0b00110000) as u16) << 4,
+                    y: r[1] as u16 | ((r[2] & 0b11000000) as u16) << 2,
+                })
+            },
+            if r[3] == 0xff && r[4] == 0xff {
+                None
+            } else {
+                Some(Self {
+                    size_or_intensity: 0,
+                    x: r[3] as u16 | ((r[2] & 0b00000011) as u16) << 8,
+                    y: r[4] as u16 | ((r[2] & 0b00001100) as u16) << 6,
+                })
+            },
+        ]
+    }
+    pub fn from_basic(r: [u8; 10]) -> [Option<Self>; 4] {
+        let [a, b] = Self::from_basic_pair([r[0], r[1], r[2], r[3], r[4]]);
+        let [c, d] = Self::from_basic_pair([r[5], r[6], r[7], r[8], r[9]]);
+        [a, b, c, d]
+    }
 }
 
 #[derive(Debug)]
@@ -154,22 +256,25 @@ impl ButtonState {
         }
     }
 }
+pub mod consts {
+    pub const IR_SENSITIVITY_LEVEL_3: [u8; 11] = [
+        0x02, 0x00, 0x00, 0x71, 0x01, 0x00, 0xaa, 0x00, 0x64, 0x63, 0x03,
+    ];
 
-mod consts {
     pub const RUMBLE: u8 = 0x10;
     pub const PLAYER_LEDS: u8 = 0x11;
     pub const DATA_REPORTING_MODE: u8 = 0x12;
-    pub const IR_CAMERA_ENABLE: u8 = 0x13;
+    pub const IR_CAMERA_PIXEL_CLOCK_ENABLE: u8 = 0x13;
     pub const SPEAKER_ENABLE: u8 = 0x14;
     pub const STATUS_INFORMATION_REQUEST: u8 = 0x15;
     pub const WRITE_MEMORY_AND_REGISTERS: u8 = 0x16;
     pub const READ_MEMORY_AND_REGISTERS: u8 = 0x17;
     pub const SPEAKER_DATA: u8 = 0x18;
     pub const SPEAKER_MUTE: u8 = 0x19;
-    pub const IR_CAMERA_ENABLE2: u8 = 0x1a;
-    pub const STATUS_INFORMATION: u8 = 0x1b;
-    pub const READ_MEMORY_AND_REGISTERS_DATA: u8 = 0x1c;
-    pub const RESULT: u8 = 0x1d;
+    pub const IR_CAMERA_CHIP_ENABLE: u8 = 0x1a;
+    pub const STATUS_INFORMATION: u8 = 0x20;
+    pub const READ_MEMORY_AND_REGISTERS_DATA: u8 = 0x21;
+    pub const RESULT: u8 = 0x22;
     pub const CORE_BUTTONS: u8 = 0x30;
     pub const CORE_BUTTONS_ACCELEROMETER: u8 = 0x31;
     pub const CORE_BUTTONS_EXTENSION8: u8 = 0x32;
